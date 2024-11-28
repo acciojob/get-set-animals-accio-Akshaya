eb87cc2969c57a1fe763dbba53aa54d94277ca2c
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-11-28 06:32:53 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 //complete this code
 class Animal {
-	constructor(species){
-		this._species = species;
-	}
-  get species() {
+    constructor(species) {
+        this._species = species;
+    }
+
+    
+    get species() {
         return this._species;
     }
- makeSound(){
-   console.log(`The ${this._species} makes a sound.`);
-  }
-}
 
-
-class Dog extends Animal {
-	
-	bark(){
-	  console.log("woof!");
-  }
+    
+    makeSound() {
+        console.log(`The ${this._species} makes a sound`);
+    }
 }
 
 
 class Cat extends Animal {
-	purr(){
-	  console.log ("purr!");
-  }
+    
+    purr() {
+        console.log("purr");
+    }
 }
 
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); 
-myDog.bark();
+
+class Dog extends Animal {
+    
+    bark() {
+        console.log("woof");
+    }
+}
+
 
 const myCat = new Cat("Siamese");
 myCat.makeSound(); 
 myCat.purr(); 
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); 
+myDog.bark(); 
+
 
 
 
